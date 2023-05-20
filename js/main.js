79d91f215d0c59363d56f769d28c29c3d8bdd48e
Dm1trous <lopic9999lokoko@gmail.com>
2023-05-20 11:02:24 +0500

    Итог

---
+++ js/main.js
@@ -0,0 +1,61 @@
+
+$(document).ready(function(){
+  $('.slider').slick({
+    dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear'
+  });
+});
+
+
+
+// const swiper = new Swiper('.swiper', {
+//     speed: 500,
+//     dots: true,
+//     infinite: true,
+//     speed: 500,
+//     pagination: {
+//         el: '.swiper-pagination',
+//       },
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//       },
+//     fade: true,
+//     cssEase: 'linear'
+//   });
+
+
+// $('.swiper-wrapper').slick({
+//     dots: true,
+//     infinite: true,
+//     speed: 500,
+//     fade: true,
+//     pagination: {
+//         el: '.swiper-pagination',
+//       },
+//     cssEase: 'linear',
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//       },
+//   });
+
+// const swiper = new Swiper('.swiper', {
+//   // $('Шангри-Ла').ready(function(){
+//   // $('.swiper').slick({
+//   dots: true,
+//     speed: 500,
+//     infinite:true,
+//     pagination: {
+//         el: '.swiper-pagination',
+//       },
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//       },
+//     fade: true,
+//     cssEase: 'cubic-bezier(0.68, -0.55, 0.265, 1.55)'
+// });
